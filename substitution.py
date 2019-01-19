5f286a425b0c0e572b5ab4ade808697c64eea5f0
Gautam Dayal <gautam.dayal@yahoo.com>
2019-01-19 09:20:01 +0530

    Update substitution.py

--- substitution.py
+++ substitution.py
@@ -8,25 +8,36 @@ def randomAlpha():
         c.append(p[x])
         p.remove(p[x])
     return c
-    
+
+# creates a keyword based mapping. eg: 'julius caesar' returns j u l i s c a e r t v w x y z b d f ...
+ def genKey(keyword):
+    a = 'abcdefghijklmnopqrstuvwxyz'
+    keylist = purify([c for c in keyword.lower()])
+    x = ord(keylist[len(keylist) - 1]) - 97
+    b = a[x:26] + a[0:x]
+    for c in b:
+        if c not in keylist:
+            keylist.append(c)
+    return(keylist)
+
 # Implementation of the substitution cipher. Specify whether to print letter mapping by saying 'True' in optional arg.
-def substitution(plaintext, givekey = False):
+# If a keyword based mapping is to be used, specify the word in the optional arg.
+ def substitution(plaintext, keyword='', givekey = False):
     plaintext = plaintext.lower()
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    L = randomAlpha()
+    if keyword == '':
+        L = randomAlpha()
+    else:
+        L = genKey(keyword)
     mapping = {}
-    
     for i in range(26):
         mapping[alphabet[i]] = L[i]
     cipherList = []
-    
     for c in plaintext:
         if c in alphabet:
             cipherList.append(mapping[c])
         else:
             cipherList.append(c)
-            
     if givekey:
-        print('key: ', mapping)    
-        
-    print('msg: '+ ''.join(cipherList))
+        print('key: \n', mapping, '\n')    
+    print('msg: ' + '\n' + ''.join(cipherList))
